0cd3d59cef103c1053ef26b30fa3f2bba2c35305
jaeeewon <hello.pleizz@gmail.com>
2025-09-17 16:51:21 +0900

    feat: print audio info if exists on get_ready_state

--- util/anns_inspector.py
+++ util/anns_inspector.py
@@ -1,14 +1,15 @@
 import os
 import ijson
+import soundfile as sf
 from typing import Callable, Any
 from collections.abc import Iterator
 
 
 def inspect_anns(
-    filename: str, fn: Callable[[dict[str, Any], dict, list], Any]
+    filename: str, fn: Callable[[dict[str, Any], dict, dict, list], Any], cfg: dict = {}
 ) -> tuple[dict, list]:
     """
-    call by reference인 dict와 list를 이용해 `fn(ann, dict, list) -> Any`의 형태로 annotation을 분석함
+    call by reference인 dict와 list를 이용해 `fn(ann, cfg, dict, list) -> Any`의 형태로 annotation을 분석함
 
     `tuple[dict, list]`의 형태로 반환
     """
@@ -17,7 +18,7 @@ def inspect_anns(
     with open(filename, "rb") as f:
         anns: Iterator[dict] = ijson.items(f, "annotation.item")
         for ann in anns:
-            fn(ann, store_dict, store_list)
+            fn(ann, cfg, store_dict, store_list)
 
     return (store_dict, store_list)
 
@@ -33,7 +34,7 @@ class Inspector:
         if train_sets is not None:
             self.train_sets = train_sets
 
-    def _count_tasks(self, ann: dict, store_dict: dict, store_list: list):
+    def _count_tasks(self, ann: dict, cfg: dict, store_dict: dict, store_list: list):
         """
         ===== salmonn_stage1_data.json =====
         {'asr': 1191381, 'audiocaption': 435174} []
@@ -49,21 +50,41 @@ class Inspector:
             store_dict[task] = 0
         store_dict[task] += 1
 
-    def _check_data_exists(self, ann: dict, store_dict: dict, store_list: list):
+    def _check_data_exists(
+        self, ann: dict, cfg: dict, store_dict: dict, store_list: list
+    ):
         task = ann.get("task")
         # {"path": "/LibriSpeech/train-clean-100/path_to_audio.flac", "text": "recognized", "task": "asr"}
         path = ann.get("path")
+        prefix = cfg.get("prefix")
         splt_path = path.split("/")
+        path_dir = "/".join(splt_path[:-1])
         dataset = splt_path[1]  # LibriSpeech
         file_name = splt_path[-1]  # path_to_audio.flac
+        real_path = f"{prefix}{path}"
 
-        key = "ready" if os.path.exists(path) else "not_ready"
+        key = "ready" if os.path.exists(real_path) else "not_ready"
 
         if task not in store_dict:
             store_dict[task] = {}
         if dataset not in store_dict[task]:
-            if key == "not_ready":
-                print(f"[{task}] {dataset} | {path} not exists!")
+            if key == "ready":
+                inspect_audio = cfg.get("inspect_audio", True)
+                if inspect_audio:
+                    try:
+                        info = sf.info(real_path)
+                        print(f"===== [{task}] {dataset} | {path} =====")
+                        print(f"file path: {path_dir}")
+                        print(f"sampling rate: {info.samplerate}")
+                        print(f"channels: {info.channels}")
+                        print(f"format: {info.format_info} ({info.format})")
+                        print(f"duration: {info.duration:.2f}s")
+                    except Exception as e:
+                        print(f"failed to load audio for {real_path}: {e}")
+            else:
+                print_not_exists = cfg.get("print_not_exists", True)
+                if print_not_exists:
+                    print(f"[{task}] {dataset} | {path_dir} required!")
             store_dict[task][dataset] = {"ready": [], "not_ready": []}
 
         store_dict[task][dataset][key].append(file_name)
@@ -74,10 +95,20 @@ class Inspector:
             store_dict, _ = inspect_anns(f"./ann/{train_set}", self._count_tasks)
             print(store_dict)
 
-    def get_ready_rate(self, print_statics=True):
+    def get_ready_rate(
+        self, print_statics=True, inspect_audio=True, print_not_exists=True, prefix="./"
+    ):
         for train_set in self.train_sets:
             print(f"===== {train_set} =====")
-            store_dict, _ = inspect_anns(f"./ann/{train_set}", self._check_data_exists)
+            store_dict, _ = inspect_anns(
+                f"./ann/{train_set}",
+                self._check_data_exists,
+                {
+                    "inspect_audio": inspect_audio,
+                    "print_not_exists": print_not_exists,
+                    "prefix": prefix,
+                },
+            )
             if print_statics:
                 for task in store_dict:
                     for dataset in store_dict[task]:
@@ -93,4 +124,9 @@ class Inspector:
 if __name__ == "__main__":
     inspector = Inspector()
     # inspector.get_stats()
-    # inspector.get_ready_rate(print_statics=False)
+    inspector.get_ready_rate(
+        print_statics=True,
+        inspect_audio=False,
+        print_not_exists=False,
+        prefix="/home/jpong/Workspace/jaeeewon",
+    )
