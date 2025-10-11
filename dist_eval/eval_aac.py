c301438868ae9e439e6ea1cea92783d8c553c25b
jaeeewon <hello.pleizz@gmail.com>
2025-10-12 02:59:00 +0900

    added: eval AAC

--- dist_eval/eval_aac.py
+++ dist_eval/eval_aac.py
@@ -1,7 +1,21 @@
-import redis, jiwer
-from nltk.translate.meteor_score import single_meteor_score, meteor_score
+import redis, jiwer, json
+from nltk.translate.meteor_score import single_meteor_score
 from statistics import mean
 
+"""
+SPIDEr = (SPICE + CIDEr) / 2
+
+# https://github.com/salaniz/pycocoevalcap
+# install requirements
+apt install openjdk-8-jdk
+java -version # openjdk version "1.8.0_462"
+pip install pycocoevalcap
+
+# error since memory limit
+# replace '-Xmx8G' with '-Xmx80G' in pycocoevalcap/spice/spice.py, line 69
+"""
+from pycocotools.coco import COCO
+from pycocoevalcap.eval import COCOEvalCap
 
 transform = jiwer.Compose(
     [
@@ -9,8 +23,7 @@ transform = jiwer.Compose(
         jiwer.RemovePunctuation(),
         jiwer.RemoveKaldiNonWords(),
         jiwer.RemoveMultipleSpaces(),
-        jiwer.Strip(),
-        jiwer.ReduceToListOfListOfWords(),
+        jiwer.Strip()
     ]
 )
 
@@ -34,31 +47,61 @@ if total_tasks == 0:
     print("no data")
     exit()
 
-answers = []
-inferences = []
+new_ref = {"images": [], "annotations": [], "info": ""}
+new_cap = []
 meteors = []
 
-for key in task_keys:
+for i, key in enumerate(task_keys, start=1):
     task_data = r.hgetall(key)
 
     answer = task_data.get("caption")
-    infer = task_data.get("infer")
+    infer = task_data.get("infer_v2")
 
     if infer.endswith("</s>"):
         infer = infer[:-4].strip()
     else:
         continue
 
-    # answers.append(transform(answer))
-    # inferences.append(transform(infer))
+    meteors.append(
+        single_meteor_score(transform(answer).split(), transform(infer).split())
+    )
+    new_ref["images"].append({"id": i})
+    new_ref["annotations"].append(
+        {"image_id": i, "id": i, "caption": transform(answer)}
+    )
+    new_cap.append({"image_id": i, "caption": transform(infer)})
 
-    meteors.append(single_meteor_score(answer.split(), infer.split()))
-    # meteor = single_meteor_score(answer.split(), infer.split())
-    # print(f"ANS: {answer}")
-    # print(f"INF: {infer}")
-    # print(f"MET: {meteor}")
-    # print("=====")
+annotation_file = "aac_annotation_ignore_.json"
+results_file = "aac_results_ignore_.json"
+with open(annotation_file, "w") as f:
+    f.write(json.dumps(new_ref))
+with open(results_file, "w") as f:
+    f.write(json.dumps(new_cap))
 
-# meteors = meteor_score(inferences, answers)
-print(mean(meteors)) # 0.19557955422256348
-# print(meteor_score(inferences, answers))
+coco = COCO(annotation_file)
+coco_result = coco.loadRes(results_file)
+coco_eval = COCOEvalCap(coco, coco_result)
+coco_eval.evaluate()
+
+# for metric, score in coco_eval.eval.items():
+#     print(f"{metric}: {score:.3f}")
+#     """
+#     Bleu_1: 0.227
+#     Bleu_2: 0.107
+#     Bleu_3: 0.055
+#     Bleu_4: 0.029
+#     METEOR: 0.108
+#     ROUGE_L: 0.217
+#     CIDEr: 0.354
+#     SPICE: 0.111
+#     """
+
+print(f"mean local meteors: {mean(meteors)}")
+# 0.2152928432061087 if not skip </s>
+# 0.21586046920735638 if skip </s>
+print(f"SPIDEr: {(coco_eval.eval['SPICE'] + coco_eval.eval['CIDEr']) / 2}")
+# 0.2326582523151935 if not skip </s>
+# 0.23334328623335548 if skip </s>
+print(
+    f"skip: {(total_tasks - len(meteors)) / total_tasks:.2f}% ({total_tasks - len(meteors)} / {total_tasks})"
+)  # skip: 0.01% (25 / 4411)
