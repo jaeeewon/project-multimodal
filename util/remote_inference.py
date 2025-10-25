96e67a26deec7cf43c51fe3fb1b6d3f004125d79
jaeeewon <hello.pleizz@gmail.com>
2025-10-26 00:53:22 +0900

    feat: prevent repetition by setting early_stopping=True in generate()

--- util/remote_inference.py
+++ util/remote_inference.py
@@ -1,5 +1,5 @@
 from omegaconf import OmegaConf
-import torch
+import torch, time, random, numpy as np, torch.backends.cudnn as cudnn
 from transformers import WhisperFeatureExtractor
 
 # <remote_import>
@@ -22,6 +22,16 @@ from salmonn.models.salmonn import SALMONN
 from salmonn.utils import prepare_one_sample
 
 # </remote_import>
+
+
+def setup_seeds(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+
 
 salmonn_cfg = {
     "ckpt": "/home/jpong/Workspace/jaeeewon/SALMONN-7B/salmonn_7b_v0.pth",
@@ -60,12 +70,65 @@ class Inference:
         with torch.cuda.amp.autocast(dtype=torch.float16):
             return self.model.generate(samples, self.config.generate, prompts=prompt)[0]
 
+    def infer_samples(self, wav_path: list[str], prompt: str):
+        samples = [prepare_one_sample(pth, self.wav_processor) for pth in wav_path]
+        spectrograms = [s["spectrogram"].squeeze() for s in samples]
+        raw_wavs = [s["raw_wav"].squeeze() for s in samples]
+        padding_masks = [s["padding_mask"].squeeze() for s in samples]
+
+        samples = {
+            "spectrogram": torch.nn.utils.rnn.pad_sequence(
+                spectrograms, batch_first=True, padding_value=0.0
+            ),
+            "raw_wav": torch.nn.utils.rnn.pad_sequence(
+                raw_wavs, batch_first=True, padding_value=0.0
+            ),
+            "padding_mask": torch.nn.utils.rnn.pad_sequence(
+                padding_masks, batch_first=True, padding_value=0.0
+            ),
+        }
+        # print("spectrogram", samples["spectrogram"].size())
+        # print("raw_wav", samples["raw_wav"].size())
+        # print("padding_mask", samples["padding_mask"].size())
+
+        prompt = [
+            self.config.model.prompt_template.format(
+                "<Speech><SpeechHere></Speech> " + prompt.strip()
+            )
+        ]
+        with torch.cuda.amp.autocast(dtype=torch.float16):
+            return self.model.generate(samples, self.config.generate, prompts=prompt)
+
 
 if __name__ == "__main__":
+    # setup_seeds(42)
     inference = Inference(config_path="configs/infer_config.yaml", device="cuda:0")
-    infered = inference.infer_one_sample(
-        "salmonn/resource/audio_demo/duck.wav",
-        "Can you transcribe the speech into a written format?",
-    )
+    # infered = inference.infer_one_sample(
+    #     "salmonn/resource/audio_demo/duck.wav",
+    #     "Can you transcribe the speech into a written format?",
+    # )
 
-    print(infered)
+    # print(infered)
+
+    for i in range(1, 51):
+        t1 = time.perf_counter()
+        print(f"===== batch={i} =====")
+        samples = [
+            "salmonn/resource/audio_demo/duck.wav",
+        ] * i
+        # samples = ["salmonn/resource/audio_demo/mountain.wav"] * i
+        # samples = random.choices(
+        #     [
+        #         "salmonn/resource/audio_demo/duck.wav",
+        #         "salmonn/resource/audio_demo/mountain.wav",
+        #     ],
+        #     k=i,
+        # )
+        infered = inference.infer_samples(
+            samples,
+            "Can you transcribe the speech into a written format?",
+            # "What do you hear?",
+        )
+        print(infered)
+        print(f"elapsed {time.perf_counter() - t1}s")
+        print()
